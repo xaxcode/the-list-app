a02db78db0876da61556277e360c5ceef1ca9877
xaxcode <olsen.zach@gmail.com>
2015-01-08 22:38:38 -0700

    new

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$("#add-to-list").click(function(){
+		var unserInput = $("#new-list-item").val();
+    $("ul").append("<li>" + userInput + "</li>");
+   
+    
+ });
+
+})
+
