4b277f23e00ce1e82248043ab82b1ced7484cfb8
xaxcode <olsen.zach@gmail.com>
2015-01-11 15:17:46 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 	$("button#add-to-list").click(function(){
-		console.log("click");
+		
 		var userInput = $("input#new-list-item").val();
 		
-    $("ul").prepend("<li>" + userInput + "</li>");
-        console.log("ul");
-    
+    $("ul#list").prepend("<li>" + userInput + "</li>");
+            
  });
 
 })
