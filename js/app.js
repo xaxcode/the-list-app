0346d088245a70652b280eb928935af42f689702
xaxcode <olsen.zach@gmail.com>
2015-01-12 21:02:41 -0700

    done

--- js/app.js
+++ js/app.js
@@ -2,7 +2,18 @@ $(document).ready(function() {
 	$("button#add-to-list").click(function(){
 		var userInput = $("input#new-list-item").val();
 	$("ul#list").prepend("<li>" + userInput + "</li>");
-            
+            event.preventDefault();
  });
+
+$( 'ul' ).on('click', 'li', function(event) {
+	$(this).toggleClass( "strike" );
+		
+});
+
+$('ul').on('dblclick', 'li', function(event) {
+		console.log("doubleclick");
+		$(this).fadeOut("slow")
+});
+
 
 })
